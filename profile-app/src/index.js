81a2fa368d41d10179f31f36e1948bd0e34d79cd
popomola <mrsimple1015@naver.com>
2021-08-01 02:31:53 +0900

    index.js

---
+++ profile-app/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+// const text = "abc def";
+
+// ReactDOM.render(<App text={text} />,document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
