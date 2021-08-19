fc6f8b85985b065456537221c85f36a94307def2
popomola <mrsimple1015@naver.com>
2021-08-19 14:57:01 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,26 @@
-import './App.css';
-import Practice from "./practice";
+import React from 'react';
+import {Grid} from '@material-ui/core';
+import CssBaseLine from '@material-ui/core/CssBaseline';
 
-function App() {
-  return (
-    <div className="App">
-      <Practice/>
-    </div>
-  );
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = ()=>{
+  return(
+      <>
+      <CssBaseLine/>
+      <Header/>
+      <Grid container spacing={3} style={{width: '100%'}}>
+        <Grid item xs={12} md={4}>
+            <List/>
+        </Grid>
+        <Grid item xs={12} md={8}>
+            <Map/>
+        </Grid>
+      </Grid>
+      </>
+  )
 }
-
 
 export default App;
