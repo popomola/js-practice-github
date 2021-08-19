71f850be655d2bd8af403744491aa7d90265c167
popomola <mrsimple1015@naver.com>
2021-08-19 14:56:42 +0900

    Create ShowStore.js

---
+++ mama-app/src/ShowStore.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const ShowStore = ()=>{
+
+    return(
+        <div>
+                <img src="https://lh3.googleusercontent.com/proxy/2a2ePRpPPjiMDQOFUL5TOAhbMTKPI37yq3N7TV55z8XSC2tCwKpoExV30TJhd48kFcNAIt_rpGDwpT0r0iSABsxB7SH5Lf44gK6HZolA7ijb2mL3d8VTUGh32UvOc8InmL8"/>
+                <span>영천문화</span>
+        </div>
+    )
+}
+
+export default ShowStore
