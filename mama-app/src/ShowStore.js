47538cc87354f77b42683803b251d38b67a4863b
popomola <mrsimple1015@naver.com>
2021-08-24 16:16:32 +0900

    Update ShowStore.js

--- mama-app/src/ShowStore.js
+++ mama-app/src/ShowStore.js
@@ -5,7 +5,7 @@ const ShowStore = ()=>{
     return(
         <div>
                 <img src="https://lh3.googleusercontent.com/proxy/2a2ePRpPPjiMDQOFUL5TOAhbMTKPI37yq3N7TV55z8XSC2tCwKpoExV30TJhd48kFcNAIt_rpGDwpT0r0iSABsxB7SH5Lf44gK6HZolA7ijb2mL3d8VTUGh32UvOc8InmL8"/>
-                <span>영천문화</span>
+                <span>영천영화</span>
         </div>
     )
 }
