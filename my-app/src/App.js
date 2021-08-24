dbddf16ff30a16e8be07f89e61f3b08956859569
popomola <mrsimple1015@naver.com>
2021-08-24 16:16:41 +0900

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
-import CssBaseLine from '@material-ui/core/CssBaseline';
-
-import Header from './components/Header/Header';
-import List from './components/List/List';
-import Map from './components/Map/Map';
+import Post from './Post'
 
 const App = ()=>{
   return(
       <>
-      <CssBaseLine/>
-      <Header/>
-      <Grid container spacing={3} style={{width: '100%'}}>
-        <Grid item xs={12} md={4}>
-            <List/>
-        </Grid>
-        <Grid item xs={12} md={8}>
-            <Map/>
-        </Grid>
-      </Grid>
+      <Post writerInfo="babo" body="babobobo"/>
       </>
   )
 }
